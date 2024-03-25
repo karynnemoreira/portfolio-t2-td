cc20f1b733f0ba8a53fdfc6aac2b31028983215b
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-03-25 15:33:31 -0300

    primeira aula de rotas, navegação

---
+++ src/Components/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Inicio(){
+return(
+    <>
+   <h2>Olá, sou o início</h2>
+    </>
+)
+}
+
+export default Inicio
---
+++ src/Components/Sobre.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Sobre(){
+return(
+    <>
+    <h2>Olá, eu sou o sobre</h2>
+    </>
+)
+}
+
+export default Sobre
